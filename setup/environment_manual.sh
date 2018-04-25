95bf8d3ad028647b711ecc5fee9fe76ce33e11ad
Pavel Petrov <itnelo@gmail.com>
2018-04-25 13:53:35 +0300

    Added manual environment setup script for deploying on new server

---
+++ setup/environment_manual.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+# Tested on: Debian 9.4
+# Linux ppetrov-prod1 4.9.0-6-amd64 #1 SMP Debian 4.9.82-1+deb9u3 (2018-03-02) x86_64 GNU/Linux
+
+# start
+sudo apt update
+
+# docker
+sudo apt install \
+     apt-transport-https \
+     ca-certificates \
+     curl \
+     gnupg2 \
+     software-properties-common
+
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
+
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/debian \
+   $(lsb_release -cs) \
+   stable"
+
+sudo apt update
+
+sudo apt install docker-ce
+
+# docker-compose
+sudo curl -L https://github.com/docker/compose/releases/download/1.21.0/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+sudo curl -L https://raw.githubusercontent.com/docker/compose/1.21.0/contrib/completion/bash/docker-compose -o /etc/bash_completion.d/docker-compose
