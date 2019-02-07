4c78eb96c3f1a683398361699588a005bf220162
Pavel Petrov <itnelo@gmail.com>
2019-02-07 21:11:11 +0300

    redis cache for app layer and doctrine

--- bin/deploy_dev.sh
+++ bin/deploy_dev.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 echo 'Stopping docker-compose services...'
+docker-compose down
 cp .env.dist .env
 cp docker-compose.yml.dist docker-compose.yml
-docker-compose down
 
 mkdir -p ./var/postgresql/data
+mkdir -p ./var/redis/data
 
 echo 'Fixing permissions...'
 source .env
@@ -18,20 +19,23 @@ echo 'Building docker-compose services...'
 docker-compose build --force-rm
 
 echo 'Updating composer dependencies...'
-docker-compose run app composer install --no-interaction
+docker-compose run --no-deps app composer install --no-interaction
 
 echo 'Updating yarn dependencies...'
 docker-compose run --no-deps app yarn install --non-interactive
 docker-compose run --no-deps app yarn run build:dev
 
 echo 'Applying migrations...'
-docker-compose run app ./bin/console doctrine:migrations:migrate --env=dev --no-interaction
+docker-compose run app ./bin/console doctrine:migrations:migrate --env=dev --no-interaction --allow-no-migration
 
-echo 'Clearing and warming up cache...'
-docker-compose run --no-deps app ./bin/console cache:clear --env=dev
+echo 'Clearing up cache...'
 docker-compose run --no-deps app ./bin/console doctrine:cache:clear-metadata --env=dev
 docker-compose run --no-deps app ./bin/console doctrine:cache:clear-result --env=dev
 docker-compose run --no-deps app ./bin/console doctrine:cache:clear-query --env=dev
+docker-compose run --no-deps app ./bin/console cache:clear --env=dev --no-warmup
+
+echo 'Warming up cache...'
+docker-compose run app ./bin/console cache:warmup --env=dev
 
 echo 'Starting docker-compose services...'
 docker-compose up -d
