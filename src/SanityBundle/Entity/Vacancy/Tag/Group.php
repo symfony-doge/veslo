d3456229539d590cb394a7736056ca46f8377475
Pavel Petrov <itnelo@gmail.com>
2019-03-05 11:22:21 +0300

    entity for group of sanity tags

---
+++ src/SanityBundle/Entity/Vacancy/Tag/Group.php
@@ -0,0 +1,131 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Veslo\SanityBundle\Entity\Vacancy\Tag;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Group for sanity tags
+ *
+ * @ORM\Table(name="sanity_vacancy_tag_group")
+ * @ORM\Entity(readOnly=true)
+ * @ORM\Cache(usage="READ_ONLY", region="index")
+ */
+class Group
+{
+    /**
+     * Identifier for group of sanity tags
+     *
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer", options={"comment": "Identifier for group of sanity tags"})
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * Sanity tags group name
+     *
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=255, unique=true, options={"comment": "Sanity tags group name"})
+     */
+    private $name;
+
+    /**
+     * Sanity tags group color
+     *
+     * @var string
+     *
+     * @ORM\Column(name="color", type="string", length=255, options={"comment": "Sanity tags group color"})
+     */
+    private $color;
+
+    /**
+     * Sanity tags group description
+     *
+     * @var string
+     *
+     * @ORM\Column(name="description", type="text", options={"comment": "Sanity tags group description")
+     */
+    private $description;
+
+    /**
+     * Returns identifier for group of sanity tags
+     *
+     * @return int
+     */
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    /**
+     * Returns sanity tags group name
+     *
+     * @return string
+     */
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    /**
+     * Sets sanity tags group name
+     *
+     * @param string $name Sanity tags group name
+     *
+     * @return void
+     */
+    public function setName(string $name): void
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * Returns sanity tags group color
+     *
+     * @return string
+     */
+    public function getColor(): string
+    {
+        return $this->color;
+    }
+
+    /**
+     * Sets sanity tags group color
+     *
+     * @param string $color Sanity tags group color
+     *
+     * @return void
+     */
+    public function setColor(string $color): void
+    {
+        $this->color = $color;
+    }
+
+    /**
+     * Returns sanity tags group description
+     *
+     * @return string
+     */
+    public function getDescription(): string
+    {
+        return $this->description;
+    }
+
+    /**
+     * Sets sanity tags group description
+     *
+     * @param string $description Sanity tags group description
+     *
+     * @return void
+     */
+    public function setDescription(string $description): void
+    {
+        $this->description = $description;
+    }
+}
