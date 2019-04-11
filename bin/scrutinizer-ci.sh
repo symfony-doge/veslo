5dcc76befc9cc9827fb5776c54fce6f62844361a
Pavel Petrov <itnelo@gmail.com>
2019-04-11 12:44:25 +0300

    integration with scrutinizer ci

--- bin/scrutinizer-ci.sh
+++ bin/scrutinizer-ci.sh
@@ -21,30 +21,50 @@ cp app/config/parameters.yml.dist app/config/parameters.yml
 cp webpack.env.dev.js.dist ./webpack.env.js
 source .env
 
+# old version of docker used by scrutinizer can't manage ARGs before FROM, so we need this hack...
+docker -v
+echo 'Fixing compatibility with legacy docker versions...'
+cp -r docker/dev docker/scrutinizer
+sed -i 's/${APP_ENV}/scrutinizer/g' docker-compose.yml
+declare -a placeholders=("PHP_VERSION" "NGINX_VERSION" "REDIS_VERSION" "RABBITMQ_VERSION")
+# removing unsupported ARGs from build context
+for placeholder in "${placeholders[@]}"
+do
+    sed -i '/'"$placeholder"':/ d' docker-compose.yml
+done
+# removing unsupported ARGs from each dockerfile
+for dockerfile in `find ./docker/scrutinizer -name 'Dockerfile'`
+do
+    for placeholder in "${placeholders[@]}"
+    do
+        sed -i '/ARG.*'"$placeholder"'/,+1 d' $dockerfile
+        sed -i 's/${'"$placeholder"'}/'"${!placeholder}"'/g' $dockerfile
+    done
+done
+
 mkdir -p ./var/postgresql/data
 mkdir -p ./var/redis/data
+mkdir -p ./var/rabbitmq
 
 echo 'Fixing permissions...'
-sudo chown 777 -R .
+sudo chown 775 -R .
 
 echo 'Building docker-compose services...'
 docker-compose build --force-rm
 
+echo 'Starting docker-compose services...'
+docker volume rm build_db_data build_redis_data build_mq_data
+docker-compose up -d db redis mq php-fpm nginx
+
 echo 'Updating composer dependencies...'
-# app -d memory_limit=-1 /usr/local/bin/composer
-docker-compose run --rm app composer install --no-interaction
+docker-compose run --rm --no-deps app composer install --no-interaction --no-scripts
 
 echo 'Updating yarn dependencies...'
 docker-compose run --rm --no-deps app yarn install --non-interactive
 docker-compose run --rm --no-deps app yarn run build:dev
 
 echo 'Applying migrations...'
-docker-compose run --rm app ./bin/console doctrine:migrations:migrate --env=dev --no-interaction --allow-no-migration
+docker-compose run --rm --no-deps app ./bin/console doctrine:migrations:migrate --env=dev --no-interaction --allow-no-migration
 
 echo 'Updating build version...'
 rm -f VERSION && docker-compose run --rm --no-deps app bin/console app:version:bump --build=`date +"%Y%m%d%H%M%S"`
-
-echo 'Starting docker-compose services...'
-docker-compose up -d
-
-docker-compose exec php-fpm php $DEPLOYMENT_PATH/bin/console about
